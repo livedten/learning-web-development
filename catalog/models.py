069c8260a311b4ee83949986c849530822eb8e30
livedten <livedten@hotmail.com>
2022-06-02 21:27:34 +0300

    Спроектируй модель
    Определение моделей LocalLibrary:
    - модель представляющая жанр книги
    - модель, представляющая книгу (но не конкретную копию книги)
    - модель, представляющая конкретную копию книги (то есть, которая может быть заимствована из библиотеки)
    - модель, представляющая автора

--- catalog/models.py
+++ catalog/models.py
@@ -1,108 +1,103 @@
 from django.db import models
-from django.urls import reverse
+from django.urls import reverse     # Используется для генерации URL-адресов путем отмены шаблонов URL-адреса
+import uuid     # Требуется для уникальных экземпляров книги
 
 # Create your models here.
 
 
-class MyModelName(models.Model):
-    """Типичный класс, определяющий модель, полученный из класса модели."""
-
-    # Поля
-    my_field_name = models.CharField(max_length=20, help_text='Введите полевую документацию',
-                                     verbose_name='мое поле имя')
-    # ...
-    """
-    Указывает, что максимальная длина значения в этом поле составляет 20 символов.
-    
-    Предоставляет текстовую метку для отображения, чтобы помочь пользователям узнать, 
-    какое значение необходимо предоставить, когда это значение должно быть введено пользователем 
-    через HTML-форму.
-    
-    По умолчанию my_field_name будет иметь метку My field name, удобочитаемое имя для поля 
-    verbose_name не должно начинаться с заглавной буквы первой буквы.
-    
-    Опции полей:
-    https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-options
-    https://django.fun/docs/django/ru/4.0/ref/models/fields/#field-options
-    
-    Типы полей:
-    https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-types
-    https://django.fun/docs/django/ru/4.0/ref/models/fields/#field-types
-    
-    Поля отношений:
-    https://docs.djangoproject.com/en/4.0/ref/models/fields/#module-django.db.models.fields.related
-    https://django.fun/docs/django/ru/4.0/ref/models/fields/#module-django.db.models.fields.related
-    
-    OneToOneField - один к одному(например, человек в стране имеет конкретный номер id num паспорта,
-    конкретный id принадлежит одному человеку). Table A(Records) -> Table B(Records)
-    
-    ForeignKey - один ко многим(например, автомобиль имеет одного производителя, но производитель 
-    может делать много автомобилей). Table A(Records) -> Table B(Records1, Records2, Records3)
-    (например, по одному адресу проживает несколько людей, но один человек имеет один адрес)
-    
-    ManyToManyField - многие ко многим(например, книга может иметь несколько жанров, и каждый жанр 
-    может содержать несколько книг)
-    (например, у одного доктора может быть множество пациентов, у пациента может быть несколько 
-    докторов)
-    """
-
-    # Метаданные
-    class Meta:
-        ordering = ['-my_field_name']
-        verbose_name = "Name"
-        verbose_name_plural = 'Names'
-    """
-    Управление сортировка записей, возвращаемых при запросе типа модели.
-    
-    verbose_name, подробное имя для класса в единственной и множественной форме
-    Если не задано, Django будет использовать verbose_name + "s"
-    
-    Мета-параметры модели(Опции модели Meta):
-    https://docs.djangoproject.com/en/4.0/ref/models/options/#model-meta-options
-    https://django.fun/docs/django/ru/4.0/ref/models/options/#model-meta-options
-    """
-
-    # Методы
-    def get_absolute_url(self):
-        """Возвращает URL, чтобы получить доступ к конкретному экземпляру MyModelName."""
-        return reverse('model-detail-view', args=[str(self.id)])
+class Genre(models.Model):
+    """Модель представляющая жанр книги."""
+    name = models.CharField(max_length=200,
+                            help_text='Введите жанр книги (например, научная фантастика)',
+                            verbose_name='жанр')
 
     def __str__(self):
-        """Строка для представления объекта MyModelName (на сайте администратора и т.д.)."""
-        return self.my_field_name
+        """Строка для представления объекта модели (на сайте администратора и т.д.)."""
+        return self.name
+
+
+class Book(models.Model):
+    """Модель, представляющая книгу (но не конкретную копию книги)."""
+    title = models.CharField(max_length=200)
+
+    # Используем Foreign Key, потому что в книге может быть только один автор, но у авторов может быть несколько книг
+    # Автор как строка, а не объект, потому что он еще не был объявлен в файле
+    # Аргументы поведения при удалении:
+    # https://django.fun/docs/django/ru/4.0/ref/models/fields/#arguments
+    # SET_NULL -- устанавливает NULL, возможно только при null=True
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, verbose_name='автор')
     """
-    Методы модели:
-    https://django.fun/docs/django/ru/4.0/ref/models/instances/#django.db.models.Model
+    Предупреждение: по умолчанию on_delete=models.CASCADE, что означает, что если автор будет удален, 
+    эта книга тоже будет удалена! Мы используем set_null здесь, но мы также могли бы использовать 
+    PROTECT или RESTRICT, чтобы предотвратить удаление автора, в то время как любая книга использует ее
     """
 
+    summary = models.TextField(max_length=1000,
+                               help_text='Введите краткое описание книги',
+                               verbose_name='краткое изложение')
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                            help_text='13 символов <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
-"""Создание и изменение записей"""
-# Создайте новую запись, используя конструктор модели.
-a_record = MyModelName(my_field_name="Instance #1")
+    # ManyToManyField используется, потому что жанр может содержать много книг. Книги могут охватывать многие жанры.
+    # Класс жанра уже определен, поэтому мы можем указать объект выше.
+    genre = models.ManyToManyField(Genre, help_text='Выберите жанр для этой книги',
+                                   verbose_name='жанр')
 
-# Сохраните объект в базу данных.
-a_record.save()
+    def __str__(self):
+        """Строка для представления объекта модели (на сайте администратора и т.д.)."""
+        return self.title
 
-# Доступ к значениям поля модели с использованием атрибутов Python.
-print(a_record.id)  # должен вернуть 1 для первой записи.
-print(a_record.my_field_name)   # должен распечатать 'Экземпляр #1'
-
-# Изменить запись, изменяя поля, а затем вызывая save().
-a_record.my_field_name = "Новое имя экземпляра"
-a_record.save()
-
-"""Поиск записей"""
-all_books = MyModelName.objects.all()
-wild_books = MyModelName.objects.filter(title__contains='wild')
-number_wild_books = MyModelName.objects.filter(title__contains='wild').count()
-
-"""
-Методы, которые возвращают новый QuerySet:
-https://docs.djangoproject.com/en/4.0/ref/models/querysets/#methods-that-return-new-querysets
-https://django.fun/docs/django/ru/4.0/ref/models/querysets/#methods-that-return-new-querysets
+    def get_absolute_url(self):
+        """Возвращает URL, чтобы получить доступ к подробной записи для этой книги."""
+        return reverse('book-detail', args=[str(self.id)])
 
 
-Поиск по полю(Поиск Field):
-https://docs.djangoproject.com/en/4.0/ref/models/querysets/#field-lookups
-https://django.fun/docs/django/ru/4.0/ref/models/querysets/#field-lookups
-"""
+class BookInstance(models.Model):
+    """Модель, представляющая конкретную копию книги (то есть, которая может быть заимствована из библиотеки)."""
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          help_text='Уникальный идентификатор этой конкретной книги по всей библиотеке')
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('о', 'На обслуживании'),
+        ('в', 'Выставлена'),
+        ('н', 'В наличии'),
+        ('р', 'Зарезервирована'),
+    )
+    status = models.CharField(max_length=1,
+                              choices=LOAN_STATUS,
+                              blank=True,
+                              default='о',
+                              help_text='Доступность книги',
+                              verbose_name='Статус')
+
+    class Meta:
+        """Сортировка по..."""
+        ordering = ['due_back']
+
+    def __str__(self):
+        """Строка для представления объекта модели (на сайте администратора и т.д.)."""
+        return '{0} ({1})'.format(self.id, self.book.title)
+
+
+class Author(models.Model):
+    """Модель, представляющая автора."""
+    first_name = models.CharField(max_length=100, verbose_name='имя')
+    last_name = models.CharField(max_length=100, verbose_name='фамилия')
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Умер', null=True, blank=True)
+
+    class Meta:
+        """Сортировка по..."""
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Возвращает URL для доступа к конкретному экземпляру автора."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """Строка для представления объекта модели (на сайте администратора и т.д.)."""
+        return '{0}, {1}'.format(self.last_name, self.first_name)

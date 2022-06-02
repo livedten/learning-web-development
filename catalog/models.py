beff67c4e00628b1f9b3891ce376f52d11698ead
livedten <livedten@hotmail.com>
2022-06-02 23:12:42 +0300

    Определи языковую модель
    Определена дополнительная модель LocalLibrary:
    - модель представляющая язык книги
    
    Выполнена миграция, обновлена БД

--- catalog/models.py
+++ catalog/models.py
@@ -18,14 +18,14 @@ class Genre(models.Model):
 
 class Book(models.Model):
     """Модель, представляющая книгу (но не конкретную копию книги)."""
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='заголовок')
 
     # Используем Foreign Key, потому что в книге может быть только один автор, но у авторов может быть несколько книг
-    # Автор как строка, а не объект, потому что он еще не был объявлен в файле
+    # 'Author' как строка, а не объект, потому что он еще не был объявлен в файле
     # Аргументы поведения при удалении:
     # https://django.fun/docs/django/ru/4.0/ref/models/fields/#arguments
     # SET_NULL -- устанавливает NULL, возможно только при null=True
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, verbose_name='автор')
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     """
     Предупреждение: по умолчанию on_delete=models.CASCADE, что означает, что если автор будет удален, 
     эта книга тоже будет удалена! Мы используем set_null здесь, но мы также могли бы использовать 
@@ -36,12 +36,14 @@ class Book(models.Model):
                                help_text='Введите краткое описание книги',
                                verbose_name='краткое изложение')
     isbn = models.CharField('ISBN', max_length=13, unique=True,
-                            help_text='13 символов <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+                            help_text='13 символов <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
 
     # ManyToManyField используется, потому что жанр может содержать много книг. Книги могут охватывать многие жанры.
     # Класс жанра уже определен, поэтому мы можем указать объект выше.
-    genre = models.ManyToManyField(Genre, help_text='Выберите жанр для этой книги',
-                                   verbose_name='жанр')
+    genre = models.ManyToManyField(Genre, help_text='Выберите жанр для этой книги')
+
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """Строка для представления объекта модели (на сайте администратора и т.д.)."""
@@ -57,6 +59,10 @@ class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True,
                           default=uuid.uuid4,
                           help_text='Уникальный идентификатор этой конкретной книги по всей библиотеке')
+    # ForeignKey для идентификации связанной книги (каждая книга может иметь много копий,
+    # но копия может иметь только одну книгу).
+    # Ключ указывает on_delete=models.RESTRICT, чтобы гарантировать, что книга не может быть удалена,
+    # когда на нее ссылается BookInstance.
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -72,7 +78,7 @@ class BookInstance(models.Model):
                               blank=True,
                               default='о',
                               help_text='Доступность книги',
-                              verbose_name='Статус')
+                              verbose_name='статус')
 
     class Meta:
         """Сортировка по..."""
@@ -101,3 +107,13 @@ class Author(models.Model):
     def __str__(self):
         """Строка для представления объекта модели (на сайте администратора и т.д.)."""
         return '{0}, {1}'.format(self.last_name, self.first_name)
+
+
+class Language(models.Model):
+    """Модель представляющая язык книги."""
+    name = models.CharField(max_length=200,
+                            help_text='Введите на каком языке написана книга (например, Беларуский)')
+
+    def __str__(self):
+        """Строка для представления объекта модели (на сайте администратора и т.д.)."""
+        return self.name
